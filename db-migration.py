e65b1a27a77ce0f29b5167faf20a482e79caa784
narsimhac <narsimhac.in@mouritech.com>
2023-07-06 10:07:37 +0530

    new updates in db-migration

--- db-migration.py
+++ db-migration.py
@@ -7,10 +7,10 @@ import botocore
 from datetime import datetime
 
 mongo_client = pymongo.MongoClient("mongodb://localhost:27017/")
-mongo_db = mongo_client["online_goods_delevery_db"]
+mongo_db = mongo_client["jhon"]
 
 dynamodb = boto3.resource('dynamodb')
-table_name_prefix = 'delivery_'
+table_name_prefix = 'mydb_data_'
 
 def convert_float_values_to_strings(data):
     if isinstance(data, float):
@@ -26,66 +26,72 @@ def convert_float_values_to_strings(data):
 
 def create_dynamodb_table(collection_name):
     table_name = table_name_prefix + collection_name
+    table_exists = table_name in [table.name for table in dynamodb.tables.all()]
 
-    try:
-        dynamodb.create_table(
-            TableName=table_name,
-            KeySchema=[
-                {
-                    'AttributeName': 'ID',
-                    'KeyType': 'HASH'
+    if not table_exists:
+        try:
+            dynamodb.create_table(
+                TableName=table_name,
+                KeySchema=[
+                    {
+                        'AttributeName': 'ID',
+                        'KeyType': 'HASH'
+                    }
+                ],
+                AttributeDefinitions=[
+                    {
+                        'AttributeName': 'ID',
+                        'AttributeType': 'S'
+                    }
+                ],
+                ProvisionedThroughput={
+                    'ReadCapacityUnits': 5,
+                    'WriteCapacityUnits': 5
                 }
-            ],
-            AttributeDefinitions=[
-                {
-                    'AttributeName': 'ID',
-                    'AttributeType': 'S'
-                }
-            ],
-            ProvisionedThroughput={
-                'ReadCapacityUnits': 5,
-                'WriteCapacityUnits': 5
-            }
-        )
-        print(f"Table '{table_name}' created successfully!")
+            )
+            print(f"Table '{table_name}' created successfully!")
 
-    except botocore.exceptions.ClientError as e:
-        if e.response['Error']['Code'] == 'ResourceInUseException':
-            print(f"Table '{table_name}' already exists.")
-        else:
-            print("An error occurred:", e)
+        except botocore.exceptions.ClientError as e:
+            if e.response['Error']['Code'] == 'ResourceInUseException':
+                print(f"Table '{table_name}' already exists.")
+            else:
+                print("An error occurred:", e)
+    else:
+        print(f"Table '{table_name}' already exists.")
 
 def migrate_collection(collection_name):
     collection = mongo_db[collection_name]
     dynamodb_table = dynamodb.Table(table_name_prefix + collection_name)
+    try:
+        data = collection.find()
+        dynamodb_items = [] 
+        for item in data:
+            item = convert_float_values_to_strings(item)
+            dynamodb_item = {}
 
-    data = collection.find()
-    dynamodb_items = [] 
-    for item in data:
-        item = convert_float_values_to_strings(item)
-        dynamodb_item = {}
+            if 'ID' in item:
+                dynamodb_item['ID'] = str(item['ID'])
+            else:
+                dynamodb_item['ID'] = 'id-v1'+ str(uuid.uuid4())
 
-        if 'ID' in item:
-            dynamodb_item['ID'] = str(item['ID'])
-        else:
-            dynamodb_item['ID'] = 'id-v1'+ str(uuid.uuid4())
+            for key, value in item.items():
+                if key == 'ID':
+                    continue
+                if isinstance(value, Decimal):
+                    value = float(value)
+                elif isinstance(value, datetime):
+                    value = value.isoformat()
+                dynamodb_item[key] = value
 
-        for key, value in item.items():
-            if key == 'ID':
-                continue
-            if isinstance(value, Decimal):
-                value = float(value)
-            elif isinstance(value, datetime):
-                value = value.isoformat()
-            dynamodb_item[key] = value
+            dynamodb_items.append(dynamodb_item)
 
-        dynamodb_items.append(dynamodb_item)
+            with dynamodb_table.batch_writer() as batch:
+                for item in dynamodb_items:
+                    batch.put_item(Item=item)
 
-        with dynamodb_table.batch_writer() as batch:
-            for item in dynamodb_items:
-                batch.put_item(Item=item)
-
-    print(f"Data migration for collection '{collection_name}' completed.")
+        print(f"Data migration for collection '{collection_name}' completed.")
+    except Exception as e:
+        print(f"An error occurred during data migration for collection '{collection_name}': {str(e)}")
 
 collection_names = mongo_db.list_collection_names()
 for collection_name in collection_names:
